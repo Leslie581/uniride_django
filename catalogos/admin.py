4a54dd611bd7357b461115931c0ef3224ddf8c6b
nancy <asl3d2y.raon@gmail.com>
2026-04-16 07:55:56 -0600

    Cambios en apps/catalogos en apps.py y admin.py

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Estado
 
-# Register your models here.
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre_estado', 'tipo_entidad')
+    list_filter = ('tipo_entidad',)
+    search_fields = ('nombre_estado',)
+    ordering = ('tipo_entidad', 'nombre_estado')
--- catalogos/apps.py
+++ catalogos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CatalogosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catalogos'
+    name = 'apps.catalogos'
