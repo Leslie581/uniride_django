f0ac992cca236ad3a9cfe8ec60cdf90e86c1d56f
Leslie Valadez <valamac2004@gmail.com>
2026-04-17 10:36:21 -0600

    Correcciones de errores

--- apps/viajes/urls.py
+++ apps/viajes/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('vehiculos/', views.mis_vehiculos, name='mis_vehiculos'),
+    path('vehiculos/', views.lista_vehiculos, name='lista_vehiculos'),
     path('vehiculos/crear/', views.crear_vehiculo, name='crear_vehiculo'),
 
     path('rutas/', views.mis_rutas, name='mis_rutas'),
     path('rutas/crear/', views.crear_ruta, name='crear_ruta'),
 
-    path('viajes/', views.listar_viajes, name='lista_viajes_disp'),
+    path('viajes/', views.listar_viajes, name='listar_viajes),
     path('viajes/crear/', views.crear_viaje, name='crear_viaje'),
     path('viajes/mis/', views.mis_viajes, name='mis_viajes'),
 
