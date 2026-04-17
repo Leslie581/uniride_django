cc0f235013f104b0075281cc60647f4e1cfd16cb
nancy <asl3d2y.raon@gmail.com>
2026-04-16 22:44:10 -0600

    templates/usuarios/inicio_pasajero.html

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     # path('logout/', views.logout_view, name='logout'),
 
-    # path('inicio/', views.inicio, name='inicio'),
+    path('inicio/', views.inicio, name='inicio'),
 
-    # path('inicio/pasajero/', views.inicio_pasajero, name='inicio_pasajero'),
+    path('inicio/pasajero/', views.inicio_pasajero, name='inicio_pasajero'),
     path('inicio/conductor/', views.inicio_conductor, name='inicio_conductor'),
 
     # path('perfil/', views.perfil, name='perfil'),
