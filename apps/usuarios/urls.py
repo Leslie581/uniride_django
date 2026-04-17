357ce4444f84421c40a1b52f86f157358468cd7c
nancy <asl3d2y.raon@gmail.com>
2026-04-16 22:46:02 -0600

    templates/usuarios/perfil.html

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -4,13 +4,13 @@ from . import views
 urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('login/', views.login_view, name='login'),
-    # path('logout/', views.logout_view, name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 
     path('inicio/', views.inicio, name='inicio'),
 
     path('inicio/pasajero/', views.inicio_pasajero, name='inicio_pasajero'),
     path('inicio/conductor/', views.inicio_conductor, name='inicio_conductor'),
 
-    # path('perfil/', views.perfil, name='perfil'),
-    # path('cambiar-rol/', views.cambiar_rol, name='cambiar_rol'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('cambiar-rol/', views.cambiar_rol, name='cambiar_rol'),
 ]
